4c7238fbbcf605a03a3200236bc6967827cd591c
Winter <sauze.thomas@gmail.com>
2015-03-12 23:50:26 +0100

    Refactoring Auth.py but still in Work In Progress. The encryption is done by libpass. I just need to fix some things inside the BOATAuth and in UserList in oder to add the csv support. CSV are actually improved basic text file. So why don't use them?

--- IRCBoat/Auth.py
+++ IRCBoat/Auth.py
@@ -1,38 +1,41 @@
 #!/usr/bin/python3.4
 #-*- coding: utf-8 -*-
 from os import linesep
-import hashlib
-import fileinput
+from passlib.hash import sha256_crypt
+import csv
 
 class User:
-    separator = ' '
+    separator = ','
 
     def __init__(self, line):
-        self.nick, self.password, self.level = line.strip().split(self.separator)
+        self.login, self.password, self.level,self.nickname =line.strip().split(
+            self.separator)
 
     def __str__(self):
-        return self.separator.join([self.nick, self.password, self.level])
+        return self.separator.join(
+            [self.login, self.password, self.level, self.nickname]
+        )
 
     def hash_password(self, password):
-        pass
+        return sha256_crypt.encrypt(password)
 
     def check_password(self, password):
-        pass
+        return self.password == hash_password(password)
 
-    def __repr__(self, password):
-        return '<{}.{} (nick: {}, level: {})>'.format(
+    def __repr__(self):
+        return '<{}.{} (login: {}, level: {})>'.format(
             self.__class__.__module__,
             self.__class__.__qualname__,
-            self.nick, self.level
+            self.login, self.level
         )
 
 class UserList(dict):
     def __init__(self, source):
         self._source = source
 
-        with open(source, 'r') as file:
+        with open(source, newline='') as file:
             for user in map(User, file.readlines()):
-                self[user.nick] = user
+                self[user.login] = user
 
     def save(self):
         with open(self._source, 'w') as file:
@@ -46,3 +49,58 @@ class UserList(dict):
             self.__class__.__qualname__,
             len(self), self._source
         )
+
+class BOATAuth:
+    def __init__(self, filename="USERS.csv"):
+        self.filename = filename
+
+    def add_user(self, login, password, level, nickname):
+        """
+        str, str, int, str ->
+        Add a new user in the USER file at self.filename.
+        Return something if the login is already used.
+        """
+        if self.is_user(login):
+            return
+        user_list = UserList(self.filename)
+        user_list[login] = User(
+            ','.join([login, password, str(level), nickname])
+        )
+        user_list.save()
+
+    def delete_user(self, login):
+        """ str ->
+        Remove the user with the given login from the USER file.
+        Do nothing if the user does not exist.
+        """
+        user_list = UserList(self.filename)
+        user_list[login] = ''
+        user_list.save()
+
+    def update_user(self, login, password=None, level=None, nickname=None):
+        """ str, str, int, str ->
+        Update the user with the given login from the USER file.
+        The args with the None values will keep the old values.
+        """
+        pass
+
+    def is_user(self, login):
+        """ str -> bool
+        Find if the user with the given login exist. Return true if yes.
+        """
+        return self.get_user(login) is not None
+
+    def is_user_connected(self, login):
+        """ str -> bool
+        Find if the user with the given login is connected. Return true if yes.
+        """
+        user_list = UserList(self.filename)
+        return user_list[login].nick is not None
+
+    def get_user(self, login):
+        """ str -> User
+        Return the user with the given login. If he does not exist, return None
+        instead.
+        """
+        user_list = UserList(self.filename)
+        return user_list[login]
